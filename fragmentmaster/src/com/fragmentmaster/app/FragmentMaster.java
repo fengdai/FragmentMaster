c5f09cfa89be7959aa8fccdf7310ef53b88ceb32
Feng Dai <toxic.dai@gmail.com>
2015-01-07 22:48:32 +0800

    Code style & Add Copyright.

--- fragmentmaster/src/com/fragmentmaster/app/FragmentMaster.java
+++ fragmentmaster/src/com/fragmentmaster/app/FragmentMaster.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2014 Feng Dai
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.fragmentmaster.app;
 
 import android.os.Bundle;
@@ -89,11 +105,7 @@ public abstract class FragmentMaster {
     }
 
     protected void setUpAnimator(IMasterFragment fragment) {
-        PageAnimator pageAnimator = null;
-        if (fragment != null) {
-            pageAnimator = fragment.onCreatePageAnimator();
-        }
-        this.setPageAnimator(pageAnimator);
+        this.setPageAnimator(fragment != null ? fragment.onCreatePageAnimator() : null);
     }
 
     protected abstract void onFragmentStarted(IMasterFragment fragment);
